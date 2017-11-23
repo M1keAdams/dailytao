330d6be50639fd4714314f0db59a572dcb7a5b0a
Mike Adams <mike.adams@uea.ac.uk>
2017-11-23 10:15:37 +0000

    Add classifications

--- setup.py
+++ setup.py
@@ -22,5 +22,15 @@ setup(
         'console_scripts': ['dailytao=dailytao.command_line:main'],
     },
     packages=['dailytao'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: System Administrators',
+        'Topic :: Other/Nonlisted Topic',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+    ],
 )
 
